82e3569833ff921f97be0f7364c11b739295fc79
CarlosNazario2010 <95858156+CarlosNazario2010@users.noreply.github.com>
2025-08-20 22:20:28 -0300

    Atualizar o Produto.java
    Arrumando a taxa do produto 3 no enum produto

--- src/main/java/com/carlosnazario/hackathon/models/Produto.java
+++ src/main/java/com/carlosnazario/hackathon/models/Produto.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public enum Produto {
     PRODUTO_1(1, "Produto 1", new BigDecimal("0.0179")),
     PRODUTO_2(2, "Produto 2", new BigDecimal("0.0175")),
-    PRODUTO_3(3, "Produto 3", new BigDecimal("0.182")),
+    PRODUTO_3(3, "Produto 3", new BigDecimal("0.0182")),
     PRODUTO_4(4, "Produto 4", new BigDecimal("0.0151"));
 
     private final int codigo;
